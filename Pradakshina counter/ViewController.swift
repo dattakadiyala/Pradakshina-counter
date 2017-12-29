f78639d3508f9942618c2b103f901d3e513adb5b
Datta <dattakadiyala@gmail.com>
2017-12-29 17:34:13 -0600

    Initial Commit

---
+++ Pradakshina counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pradakshina counter
+//
+//  Created by Datta on 12/29/17.
+//  Copyright © 2017 dvapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
