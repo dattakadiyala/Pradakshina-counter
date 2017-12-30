2882a2ad363bb68408f88f1e3ae662807c8bb876
Datta <dattakadiyala@gmail.com>
2017-12-30 02:04:26 -0600

    First Commit

--- Pradakshina counter/ViewController.swift
+++ Pradakshina counter/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    let impact = UIImpactFeedbackGenerator()
+    var counterNumbers: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        counterLabel.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func incrementCount(_ sender: Any) {
+        self.counterNumbers += 1
+        self.counterLabel.text = String(self.counterNumbers)
+        impact.impactOccurred()
     }
-
+    
+    @IBAction func decrementCount(_ sender: Any) {
+        impact.impactOccurred()
+        self.counterNumbers -= 1
+        self.counterLabel.text = String(self.counterNumbers)
+    }
+    
+    @IBAction func resetCount(_ sender: Any) {
+        let alert = UIAlertController(title: "Are you sure !", message: "to reset", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.destructive, handler: { action in
+            self.counterNumbers = 0
+            self.counterLabel.text = String(self.counterNumbers)
+        }))
+        alert.addAction(UIAlertAction(title: "cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 
 }
+
 
